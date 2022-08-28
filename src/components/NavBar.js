0d96e585b74db60e7f236edc7e3b6d0e22edc9a6
Jonathan <jonathanmoorevla@gmail.com>
2022-08-27 21:25:49 -0500

    Expant components, pass tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkTags = links.map((link) => {
+    return <a key={link + 'Key'} href={'#' + link} >{link}</a>
+  })
+  console.log(linkTags)
+  return <nav>{linkTags}</nav>;
 }
 
 export default NavBar;
